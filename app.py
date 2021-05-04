9191077d677a349848c7597e5a2ef8407e56855f
gnaga <giridhar.naga@gmail.com>
2021-05-04 20:35:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello RBBN!'
+def index():
+    return "<h1>Hello, World!</h1>"
+
+@app.route('/user/<name>')
+def user(name):
+        return '<h1>Hello, {0}!</h1>'.format(name)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True)
